69400955fb1afadf5bf91ebecbfd2d2661dd45e5
vizu_cp <SND\vizu_cp>
2013-03-26 22:49:52 +0000

    AssemblyInfo cleanup.

--- 1.0/source/NsDepCop.MsBuildTask/Properties/AssemblyInfo.cs
+++ 1.0/source/NsDepCop.MsBuildTask/Properties/AssemblyInfo.cs
@@ -1,36 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("NsDepCop.MsBuildTask")]
 [assembly: AssemblyDescription("Custom MsBuild task that runs the NsDepCop tool as part of the build process.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Codartis")]
-[assembly: AssemblyProduct("NsDepCop")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b8b21b12-66cc-4334-8508-2bfc927058bd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 1.0/source/include/CommonAssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Codartis")]
+[assembly: AssemblyProduct("NsDepCop")]
+[assembly: AssemblyCopyright("Copyright © Codartis 2013")]
