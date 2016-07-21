ace0deba1528d8ddcb4f27270a5afae6f568735a
Ferenc Vizkeleti <realvizu@users.noreply.github.com>
2016-07-21 05:36:29 +0000

    TestHost measures run time.

--- source/NsDepCop.TestHost/Program.cs
+++ source/NsDepCop.TestHost/Program.cs
@@ -15,21 +15,54 @@ namespace Codartis.NsDepCop.TestHost
     {
         public static int Main(string[] args)
         {
-            if (args.Length != 1)
+            if (args.Length < 1 || args.Length > 2)
             {
                 Usage();
                 return -1;
             }
 
+            var parserType = GetParserType(args);
+
             var csProjFileName = args[0];
             var csProjParser = new CsProjParser(csProjFileName);
             var configFileName = Path.Combine(Path.GetDirectoryName(csProjFileName), "config.nsdepcop");
 
-            var analyzer = DependencyAnalyzerFactory.Create(ParserType.Roslyn, configFileName);
-            var dependencyViolations = analyzer.AnalyzeProject(csProjParser.SourceFilePaths, csProjParser.ReferencedAssemblyPaths).ToList();
-            DumpDependencyViolations(dependencyViolations);
+            Console.WriteLine($"Analysing {csProjFileName} with {parserType}...");
+
+            var runTimeSpans = new List<TimeSpan>();
+            for (int i = 0; i < 4; i++)
+                runTimeSpans.Add(AnalyseCsProj(parserType, configFileName, csProjParser));
+
+            DumpRunTimes(runTimeSpans);
 
             return 0;
+        }
+
+        private static ParserType GetParserType(string [] args)
+        {
+            var parserType = ParserType.Roslyn;
+            if (args.Length == 2)
+            {
+                if (!Enum.TryParse(args[1], out parserType))
+                    Console.WriteLine($"Cannot parse '{args[1]}' ParserType, using {parserType} as default.");
+            }
+            return parserType;
+        }
+
+        private static TimeSpan AnalyseCsProj(ParserType parserType, string configFileName, CsProjParser csProjParser)
+        {
+            var startTime = DateTime.Now;
+
+            var analyzer = DependencyAnalyzerFactory.Create(parserType, configFileName);
+            var dependencyViolations = analyzer.AnalyzeProject(csProjParser.SourceFilePaths, csProjParser.ReferencedAssemblyPaths).ToList();
+
+            var endTime = DateTime.Now;
+            var elapsedTimeSpan = endTime - startTime;
+            Console.WriteLine($"Analysis took: {elapsedTimeSpan:mm\\:ss\\.fff}");
+
+            DumpDependencyViolations(dependencyViolations);
+
+            return elapsedTimeSpan;
         }
 
         private static void DumpDependencyViolations(IReadOnlyCollection<DependencyViolation> dependencyViolations)
@@ -37,6 +70,12 @@ namespace Codartis.NsDepCop.TestHost
             Console.WriteLine($"DependencyViolations.Count={dependencyViolations.Count}");
             foreach (var dependencyViolation in dependencyViolations)
                 Console.WriteLine(Constants.IllegalDependencyIssue.GetDynamicDescription(dependencyViolation));
+        }
+
+        private static void DumpRunTimes(List<TimeSpan> runTimeSpans)
+        {
+            var averageTimeSpan = TimeSpan.FromMilliseconds(runTimeSpans.Skip(1).Average(i => i.TotalMilliseconds));
+            Console.WriteLine($"Average run time (skipping 1st):{averageTimeSpan:mm\\:ss\\.fff}");
         }
 
         private static void Usage()
--- source/measure.cmd
+++ source/measure.cmd
@@ -1,3 +1,5 @@
-call timecmd .\NsDepCop.TestHost\bin\release\NsDepCop.TestHost.exe "c:\Work\LinqOverCSharp\CSharpFactoryVS2010\CSharpTreeBuilder\CSharpTreeBuilder.csproj"
- 
-call timecmd .\NsDepCop.TestHost\bin\release\NsDepCop.TestHost.exe "c:\Temp\NRefactory-master\ICSharpCode.NRefactory.CSharp\ICSharpCode.NRefactory.CSharp.csproj"
+.\NsDepCop.TestHost\bin\release\NsDepCop.TestHost.exe "c:\Temp\NRefactory-master\ICSharpCode.NRefactory.CSharp\ICSharpCode.NRefactory.CSharp.csproj" Roslyn
+.\NsDepCop.TestHost\bin\release\NsDepCop.TestHost.exe "c:\Temp\NRefactory-master\ICSharpCode.NRefactory.CSharp\ICSharpCode.NRefactory.CSharp.csproj" NRefactory
+
+.\NsDepCop.TestHost\bin\release\NsDepCop.TestHost.exe "c:\Work\LinqOverCSharp\CSharpFactoryVS2010\CSharpTreeBuilder\CSharpTreeBuilder.csproj" Roslyn
+.\NsDepCop.TestHost\bin\release\NsDepCop.TestHost.exe "c:\Work\LinqOverCSharp\CSharpFactoryVS2010\CSharpTreeBuilder\CSharpTreeBuilder.csproj" NRefactory
