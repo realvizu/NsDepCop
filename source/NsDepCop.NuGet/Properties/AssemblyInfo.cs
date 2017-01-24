e3a6af9592196b84c23d8573b859297471b616e2
realvizu <vizkeletiferenc@gmail.com>
2017-01-24 15:23:56 +0100

    Added project that creates NsDepCop nuget package.

---
+++ source/NsDepCop.NuGet/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("NsDepCop.NuGet")]
+[assembly: AssemblyDescription("Dummy assembly; the purpose of this project is to create a NuGet package.")]
--- source/include/CommonAssemblyInfo.cs
+++ source/include/CommonAssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: AssemblyCompany("Codartis")]
 [assembly: AssemblyProduct("NsDepCop")]
-[assembly: AssemblyCopyright("Copyright © Codartis 2013-2016")]
+[assembly: AssemblyCopyright("Copyright © 2013-2017 Ferenc Vizkeleti")]
