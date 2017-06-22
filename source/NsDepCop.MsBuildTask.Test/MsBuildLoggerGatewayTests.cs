33da783ebffa71639cfd2ad48fe00fa4518531f8
realvizu <vizkeletiferenc@gmail.com>
2017-06-22 16:54:51 +0200

    Uni tested MsBuildLoggerGateway.

---
+++ source/NsDepCop.MsBuildTask.Test/MsBuildLoggerGatewayTests.cs
@@ -0,0 +1,86 @@
+﻿using Codartis.NsDepCop.Core.Interface.Analysis;
+using Codartis.NsDepCop.Core.Interface.Config;
+using Microsoft.Build.Framework;
+using Moq;
+using Xunit;
+
+namespace Codartis.NsDepCop.MsBuildTask.Test
+{
+    public class MsBuildLoggerGatewayTests
+    {
+        private readonly Mock<IBuildEngine> _buildEngineMock = new Mock<IBuildEngine>();
+
+        [Fact]
+        public void LogTraceMessage_Success()
+        {
+            CreateLogger().LogTraceMessage(new[] { "aaa", "bbb" });
+
+            VerifyLogMessageEvent("aaa", MessageImportance.Low);
+            VerifyLogMessageEvent("bbb", MessageImportance.Low);
+        }
+
+        [Fact]
+        public void LogIssue_Error_Success()
+        {
+            var issue = new IssueDescriptor("MyCode", IssueKind.Error, "MyDescription");
+
+            CreateLogger().LogIssue(issue);
+
+            VerifyLogErrorEvent("MyCode", "MyDescription");
+        }
+
+        [Fact]
+        public void LogIssue_Warning_Success()
+        {
+            var issue = new IssueDescriptor("MyCode", IssueKind.Warning, "MyDescription");
+
+            CreateLogger().LogIssue(issue);
+
+            VerifyLogWarningEvent("MyCode", "MyDescription");
+        }
+
+        [Fact]
+        public void LogIssue_Info_Success()
+        {
+            var issue = new IssueDescriptor("MyCode", IssueKind.Info, "MyDescription");
+
+            CreateLogger().LogIssue(issue);
+
+            VerifyLogMessageEvent("MyDescription", MessageImportance.Normal);
+        }
+
+        [Fact]
+        public void LogIssue_Info_HighImportance_Success()
+        {
+            var issue = new IssueDescriptor("MyCode", IssueKind.Info, "MyDescription");
+
+            var logger = CreateLogger();
+            logger.InfoImportance = MessageImportance.High;
+            logger.LogIssue(issue);
+
+            VerifyLogMessageEvent("MyDescription", MessageImportance.High);
+        }
+
+        [Fact]
+        public void LogIssueT_Success()
+        {
+            var issue = new IssueDescriptor<string>("MyCode", IssueKind.Info, "MyDescription", i => i);
+
+            CreateLogger().LogIssue(issue, "MyParameter");
+
+            VerifyLogMessageEvent("MyParameter", MessageImportance.Normal);
+        }
+
+        private MsBuildLoggerGateway CreateLogger() => new MsBuildLoggerGateway(_buildEngineMock.Object);
+
+        private void VerifyLogMessageEvent(string message, MessageImportance messageImportance) =>
+            _buildEngineMock.Verify(
+                i => i.LogMessageEvent(It.Is<BuildMessageEventArgs>(e => e.Message.Contains(message) && e.Importance == messageImportance)));
+
+        private void VerifyLogWarningEvent(string code, string message) =>
+            _buildEngineMock.Verify(i => i.LogWarningEvent(It.Is<BuildWarningEventArgs>(e => e.Code == code && e.Message.Contains(message))));
+
+        private void VerifyLogErrorEvent(string code, string message) =>
+            _buildEngineMock.Verify(i => i.LogErrorEvent(It.Is<BuildErrorEventArgs>(e => e.Code == code && e.Message.Contains(message))));
+    }
+}
--- source/NsDepCop.MsBuildTask.Test/NsDepCopTaskTests.cs
+++ source/NsDepCop.MsBuildTask.Test/NsDepCopTaskTests.cs
@@ -86,6 +86,16 @@ namespace Codartis.NsDepCop.MsBuildTask.Test
         }
 
         [Fact]
+        public void TaskInfoImportanceIsLow()
+        {
+            var nsDepCopTask = CreateNsDepCopTask();
+            nsDepCopTask.InfoImportance = new TestTaskItem("Low");
+            nsDepCopTask.Execute().Should().BeTrue();
+
+            _loggerMock.VerifySet(i => i.InfoImportance = MessageImportance.Low);
+        }
+
+        [Fact]
         public void TooManyIssues()
         {
             CreateNsDepCopTask().Execute().Should().BeTrue();
@@ -145,6 +155,16 @@ namespace Codartis.NsDepCop.MsBuildTask.Test
 
             VerifyTaskExceptionLogged(Times.Never());
             VerifyConfigExceptionLogged(Times.Once());
+        }
+
+        [Fact]
+        public void UnexpectedException_TaskExceptionLogged()
+        {
+            _loggerMock.Setup(i => i.LogTraceMessage(It.IsAny<IEnumerable<string>>())).Throws<Exception>();
+
+            CreateNsDepCopTask().Execute().Should().BeFalse();
+
+            VerifyTaskExceptionLogged(Times.Once());
         }
 
         private NsDepCopTask CreateNsDepCopTask([CallerMemberName] string taskName = null)
