14a952209125873067996491ecb6dca9c9edf43d
realvizu <vizkeletiferenc@gmail.com>
2017-07-08 11:09:51 +0200

    Dogfooding nuget 1.7.1-rc1.

--- source/NsDepCop.VisualStudioIntegration.Vs2015/NsDepCopDiagnosticAnalyzerBase.cs
+++ source/NsDepCop.VisualStudioIntegration.Vs2015/NsDepCopDiagnosticAnalyzerBase.cs
@@ -209,6 +209,6 @@ namespace Codartis.NsDepCop.VisualStudioIntegration
             vsActivityLog.LogEntry((uint)__ACTIVITYLOG_ENTRYTYPE.ALE_ERROR, ProductConstants.ToolName, exception.ToString());
         }
 
-        protected static void LogTraceMessage(string message) => Trace.WriteLine($"[{ProductConstants.ToolName}] {message}");
+        protected static void LogTraceMessage(string message) => Debug.WriteLine($"[{ProductConstants.ToolName}] {message}");
     }
 }
