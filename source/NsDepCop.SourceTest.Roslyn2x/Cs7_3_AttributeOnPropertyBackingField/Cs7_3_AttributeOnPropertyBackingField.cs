cc29e14f4709c1cadda9a79e19e350b7ddcb8821
realvizu <vizkeletiferenc@gmail.com>
2018-06-24 18:15:26 +0200

    Added a test for a C# 7.3 feature (attribute on property-backing field).

---
+++ source/NsDepCop.SourceTest.Roslyn2x/Cs7_3_AttributeOnPropertyBackingField/Cs7_3_AttributeOnPropertyBackingField.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace A
+{
+    [Serializable]
+    public class Class1
+    {
+        [field: NonSerialized]
+        public Class1 P1 { get; set; }
+    }
+}
---
+++ source/NsDepCop.SourceTest.Roslyn2x/Cs7_3_Tests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace Codartis.NsDepCop.SourceTest
+{
+    /// <summary>
+    /// Tests that the analyzer handles various C# 7.3 constructs correctly.
+    /// </summary>
+    /// <remarks>
+    /// The name of the source file and its containing folder is the same as the name of the test.
+    /// </remarks>
+    // ReSharper disable once InconsistentNaming
+    public class Cs7_3_Tests
+    {
+        [Fact]
+        public void Cs7_3_AttributeOnPropertyBackingField()
+        {
+            SourceTestSpecification.Create()
+
+                // [Serializable]
+                .ExpectInvalidSegment(5, 6, 18)
+
+                // [field: NonSerialized]
+                .ExpectInvalidSegment(8, 17, 30)
+
+                .Execute();
+        }
+    }
+}
