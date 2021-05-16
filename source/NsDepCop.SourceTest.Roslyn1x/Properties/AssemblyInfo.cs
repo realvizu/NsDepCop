c159552f11452bc9313af531c887f5bdc35ddc7c
Ferenc Vizkeleti <vizkeletiferenc@gmail.com>
2021-05-16 16:14:03 +0200

    Deleted SourceTests.Roslyn1x and renamed Roslyn2x to just Roslyn.

--- source/NsDepCop.SourceTest.Roslyn1x/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("NsDepCop.SourceTest.Roslyn1x")]
-[assembly: AssemblyDescription("Tests the dependency analyzer with source files using Roslyn 1.x parser.")]
