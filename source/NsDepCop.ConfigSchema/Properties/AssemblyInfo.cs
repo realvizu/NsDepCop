bb6ed8effe0221dd50fe2a2496d07be867daeef5
Ferenc Vizkeleti <vizkeletiferenc@gmail.com>
2023-05-24 22:17:27 +0200

    Deleted leftover files of the late XML config schema VSIX project. Added the XML config schema files to the solution in a solution folder.

--- source/NsDepCop.ConfigSchema/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("NsDepCop_ConfigSchema")]
-[assembly: AssemblyDescription("VSIX package that adds NsDepCop config XML schema support to Visual Studio.")]
